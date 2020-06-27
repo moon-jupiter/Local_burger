658907d57f6141b0df9f252ec32fce77e61a37c9
moonjeonggyu <65287555+moonjeonggyu@users.noreply.github.com>
2020-06-27 17:39:35 +0900

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -9,7 +9,7 @@ import os
 
 
 def import_content(filepath):
-    client = MongoClient('mongodb://test:test@127.0.0.1',27017) db = client.dbsparta
+    client = MongoClient('mongodb://test:test@127.0.0.1',27017)
     mng_db = client['dbsparta'] #Replace mongo db name
     collection_name = 'localburger' #Replace mongo db collection name
     db_cm = mng_db[collection_name]
